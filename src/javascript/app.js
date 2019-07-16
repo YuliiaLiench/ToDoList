e0dac0dad697804a4b6da2f6090970ab9b9b3665
Yuliia Liench <julia.liashenko03@gmail.com>
2019-07-16 21:03:52 +0300

    create project

---
+++ src/javascript/app.js
@@ -0,0 +1,15 @@
+
+class App {
+  constructor() {
+    this.startApp();
+  }
+
+  static rootElement = document.getElementById('root');
+  static loadingElement = document.getElementById('loading-overlay');
+
+  async startApp() {
+
+  }
+}
+
+export default App;
